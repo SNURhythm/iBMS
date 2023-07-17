af74777d8bda20545d72c17cd820d078769eb182
VioletXF <zudevxf@gmail.com>
2023-07-18 02:02:28 +0900

    Create BMSRenderer

---
+++ Assets/Scripts/BMSRenderer.cs
@@ -0,0 +1,11 @@
+﻿
+public class BMSRenderer
+{
+    private readonly Chart chart;
+    private readonly RhythmControl rhythmControl;
+    BMSRenderer(Chart chart, RhythmControl rhythmControl)
+    {
+        this.chart = chart;
+        this.rhythmControl = rhythmControl;
+    }
+}
