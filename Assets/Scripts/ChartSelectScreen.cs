9531d19b72575fb0404f5a5cd42334ca59acab11
VioletXF <zudevxf@gmail.com>
2023-08-13 21:44:16 +0900

    [WIP] Chart select screen ui

--- Assets/Scripts/ChartSelectScreen.cs
+++ Assets/Scripts/ChartSelectScreen.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using B83.Image.BMP;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.UI;
@@ -15,60 +16,63 @@ public class ChartSelectScreen : MonoBehaviour
     private GameObject ChartSelectButtonPrefab;
     [SerializeField]
     private GameObject ChartSelectButtonParent;
-
+    [SerializeField]
+    private RawImage Background;
     [SerializeField] private Toggle AutoToggle;
     [SerializeField] private Toggle KeySoundToggle;
     private GameObject[] ChartSelectButtons;
     // Start is called before the first frame update
     void Start()
     {
-
+        var bmpLoader = new BMPLoader();
+        var image = bmpLoader.LoadBMP(Application.persistentDataPath + "/take003/bga_take_bg.bmp");
+        var texture = image.ToTexture2D();
+        Background.texture = texture;
     }
-    
+
     private void OnEnable()
     {
 
         TouchSimulation.Enable();
-
-        // NOTE: This would not work on Android but it's fine for now since we'll not be using StreamingAssets on release
-        var info = new DirectoryInfo(Application.persistentDataPath);
-        var fileInfo = info.GetDirectories();
-        ChartSelectButtons = new GameObject[fileInfo.Length];
-        for (var i = 0; i < fileInfo.Length; i++)
-        {
-            var file = fileInfo[i];
-
-            if (file.Name.EndsWith(".meta")) continue;
-            var button = Instantiate(ChartSelectButtonPrefab, ChartSelectButtonParent.transform);
-            ChartSelectButtons[i] = button;
-            button.GetComponent<ChartSelectButton>().ChartTitle.text = file.Name;
-            button.GetComponent<Button>().onClick.AddListener(() =>
-            {
-                // peek the directory and add buttons of *.bms, *.bme, *.bml, *.pms
-                var chartInfo = new DirectoryInfo(file.FullName);
-                var chartFileInfo = chartInfo.GetFiles();
-                foreach (var chartFile in chartFileInfo)
-                {
-                    if (!new [] { ".bms", ".bme", ".bml" }.Contains(chartFile.Extension)) continue;
-                    var chartButton = Instantiate(ChartSelectButtonPrefab, ChartSelectButtonParent.transform);
-                    chartButton.GetComponent<ChartSelectButton>().ChartTitle.text = chartFile.Name;
-                    chartButton.GetComponent<Button>().onClick.AddListener(() =>
-                    {
-                        Debug.Log("Load chart: " + chartFile.FullName);
-                        GameManager.Instance.BmsPath = chartFile.FullName;
-                        GameManager.Instance.AutoPlay = AutoToggle.isOn;
-                        GameManager.Instance.KeySound = KeySoundToggle.isOn;
-                        // load scene
-                        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("LoadingScene");
-                    });
-                }
-            });
-        }
+        //
+        // var info = new DirectoryInfo(Application.persistentDataPath);
+        // var fileInfo = info.GetDirectories();
+        // ChartSelectButtons = new GameObject[fileInfo.Length];
+        // for (var i = 0; i < fileInfo.Length; i++)
+        // {
+        //     var file = fileInfo[i];
+        //
+        //     if (file.Name.EndsWith(".meta")) continue;
+        //     var button = Instantiate(ChartSelectButtonPrefab, ChartSelectButtonParent.transform);
+        //     ChartSelectButtons[i] = button;
+        //     button.GetComponent<ChartSelectButton>().ChartTitle.text = file.Name;
+        //     button.GetComponent<Button>().onClick.AddListener(() =>
+        //     {
+        //         // peek the directory and add buttons of *.bms, *.bme, *.bml, *.pms
+        //         var chartInfo = new DirectoryInfo(file.FullName);
+        //         var chartFileInfo = chartInfo.GetFiles();
+        //         foreach (var chartFile in chartFileInfo)
+        //         {
+        //             if (!new [] { ".bms", ".bme", ".bml" }.Contains(chartFile.Extension)) continue;
+        //             var chartButton = Instantiate(ChartSelectButtonPrefab, ChartSelectButtonParent.transform);
+        //             chartButton.GetComponent<ChartSelectButton>().ChartTitle.text = chartFile.Name;
+        //             chartButton.GetComponent<Button>().onClick.AddListener(() =>
+        //             {
+        //                 Debug.Log("Load chart: " + chartFile.FullName);
+        //                 GameManager.Instance.BmsPath = chartFile.FullName;
+        //                 GameManager.Instance.AutoPlay = AutoToggle.isOn;
+        //                 GameManager.Instance.KeySound = KeySoundToggle.isOn;
+        //                 // load scene
+        //                 UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("LoadingScene");
+        //             });
+        //         }
+        //     });
+        // }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
